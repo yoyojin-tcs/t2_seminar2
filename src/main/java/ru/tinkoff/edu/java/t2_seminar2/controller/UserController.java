752e7a2845b4b2e43791a8e70b05dbe3d7774619
yoyojin <a.o.korotkov.tcs@gmail.com>
2024-02-19 19:03:19 +0300

    add swagger

--- src/main/java/ru/tinkoff/edu/java/t2_seminar2/controller/UserController.java
+++ src/main/java/ru/tinkoff/edu/java/t2_seminar2/controller/UserController.java
@@ -1,12 +1,9 @@
 package ru.tinkoff.edu.java.t2_seminar2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.tinkoff.edu.java.t2_seminar2.dto.UserList;
 import ru.tinkoff.edu.java.t2_seminar2.service.UserService;
 
 @RestController
